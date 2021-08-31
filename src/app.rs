1a63cbed91a4f15e985bcbd7e6852ea56a42cf84
Tahara Yoshinori <read.eval.print@gmail.com>
2021-08-31 20:55:27 +0900

    fetch

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,23 @@
-use yew::{html, Component, ComponentLink, Html, MouseEvent, ShouldRender};
+use anyhow::Error;
+use yew::{
+    format::{Json, Nothing},
+    html,
+    services::{
+        fetch::{FetchTask, Request, Response},
+        FetchService,
+    },
+    Component, ComponentLink, Html, MouseEvent, ShouldRender,
+};
 
 pub struct Model {
     link: ComponentLink<Self>,
     counter: i32,
+    foo: String,
+    _fetch_task: FetchTask,
 }
 
 pub enum Msg {
+    DidFetchFriends(Response<Json<Result<String, Error>>>),
     Click(MouseEvent),
 }
 
@@ -14,7 +26,17 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, counter: 0 }
+        let request = Request::get("https://vrchat.com/api/1/auth/user/friends")
+            .body(Nothing)
+            .unwrap();
+        let callback = link.callback(Msg::DidFetchFriends);
+        let _fetch_task = FetchService::fetch(request, callback).unwrap();
+        Self {
+            link,
+            counter: 0,
+            foo: "ふぇっち".to_string(),
+            _fetch_task,
+        }
     }
 
     fn change(&mut self, _: Self::Properties) -> ShouldRender {
@@ -23,6 +45,22 @@ impl Component for Model {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
+            Msg::DidFetchFriends(response) => {
+                let (meta, Json(json_data)) = response.into_parts();
+                if meta.status.is_success() || meta.status.as_u16() == 304 {
+                    match json_data {
+                        Ok(out) => {
+                            self.foo = out;
+                        }
+                        Err(_error) => {
+                            // utils::handle_api_error(Some(error));
+                        }
+                    }
+                } else {
+                    // utils::handle_api_error(None);
+                }
+                true
+            }
             Msg::Click(event) => {
                 event.prevent_default();
                 self.counter += 1;
@@ -36,6 +74,7 @@ impl Component for Model {
         html! {
           <div>
             <div>{"に～ぼし"}</div>
+            <div>{&self.foo}</div>
             <div>{self.counter}</div>
             <div><button onclick=click>{"++"}</button></div>
           </div>
